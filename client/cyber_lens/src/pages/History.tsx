b071069a104f8d8d74e300e735fa6c32016636e2
ISHAN RAJ SINGH <srajishan21@gmail.com>
2025-12-26 21:48:59 +0530

    Create History.tsx

---
+++ client/cyber_lens/src/pages/History.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const History: React.FC = () => {
+  return (
+    <div className="page-container">
+      <h1>History</h1>
+      <p>View your search and analysis history</p>
+    </div>
+  );
+};
+
+export default History;
