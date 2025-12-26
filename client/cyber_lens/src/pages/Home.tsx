3917fb3393f91152a53b0c9a1bd1ae8671512a94
ISHAN RAJ SINGH <srajishan21@gmail.com>
2025-12-26 21:48:41 +0530

    Add Home component for Cyber Lens application

---
+++ client/cyber_lens/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="page-container">
+      <h1>Welcome to Cyber Lens</h1>
+      <p>Your cybersecurity intelligence platform</p>
+    </div>
+  );
+};
+
+export default Home;
