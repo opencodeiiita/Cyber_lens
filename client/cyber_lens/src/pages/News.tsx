fb21e4216f7d38f4fec94a5aadee80e7a539b89b
ISHAN RAJ SINGH <srajishan21@gmail.com>
2025-12-26 21:49:19 +0530

    Add News component for cybersecurity updates

---
+++ client/cyber_lens/src/pages/News.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const News: React.FC = () => {
+  return (
+    <div className="page-container">
+      <h1>Cybersecurity News</h1>
+      <p>Latest updates and threat intelligence</p>
+    </div>
+  );
+};
+
+export default News;
