47f9e82b89b107e4dd77b60c54af04187eeefa69
ISHAN RAJ SINGH <srajishan21@gmail.com>
2025-12-26 21:51:37 +0530

    Refactor App component to use React Router

--- client/cyber_lens/src/App.tsx
+++ client/cyber_lens/src/App.tsx
@@ -1,16 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import History from './pages/History';
+import News from './pages/News';
+import './App.css';
 
-
-import './App.css'
-
-function App() {
-
-
+const App: React.FC = () => {
   return (
-    <>
-      {/* Start Here */}
-    
-    </>
-  )
-}
+    <Router>
+      <div className="app">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
